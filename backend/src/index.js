32dd2f09eca48f387a77786aff625d9c5673d747
otaviopetry <otaviopetry@gmail.com>
2020-06-15 23:13:37 -0300

    Basic routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/projects', (request, response) => {
     return response.json({
-        message: 'Hello world.'
+        message: 'Hello, dude!'
     });
 })
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Project 1',
+        'Project 2',
+        'Project 3'
+    ])
+})
+
+app.put('/projects/:id', (request, response) => {
+
+    return response.json({
+        message: `Project ${request.params.id} has been updated.`
+    })
+})
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json({
+        message: `Project ${request.params.id} has been deleted.`
+    })
+})
+
 
 app.listen('3333', () => {
     console.log(' 🚀 The server is up and running, dude :D');
